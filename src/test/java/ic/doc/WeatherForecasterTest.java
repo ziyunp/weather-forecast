4d85bf702faa8bc91727b8a6eccf4703990298e3
Ziyunp <ziyun_poh@hotmail.com>
2020-03-06 16:19:10 +0000

    Check for invalid arguments when querying for forecast in the test

--- src/test/java/ic/doc/WeatherForecasterTest.java
+++ src/test/java/ic/doc/WeatherForecasterTest.java
@@ -8,15 +8,34 @@ import org.junit.Test;
 public class WeatherForecasterTest {
   final WeatherForecaster forecaster = new WeatherForecaster();
   @Test
-  public void canGetForecastByRegionAndDay() {
+  public void canGetForecastByRegionAndDayWithStringsOfAnyLetterCase() {
 
-    Forecast londonForecast = forecaster.getForecastFor("LONDON", "MONDAY");
+    Forecast londonForecast = forecaster.getForecastFor("London", "Monday");
     System.out.println("London outlook: " + londonForecast.summary());
     System.out.println("London temperature: " + londonForecast.temperature());
 
-    Forecast edinburghForecast = forecaster.getForecastFor("EDINBURGH", "MONDAY");
+    Forecast edinburghForecast = forecaster.getForecastFor("edinburgh", "SUNDAY");
 
     System.out.println("Edinburgh outlook: " + edinburghForecast.summary());
     System.out.println("Edinburgh temperature: " + edinburghForecast.temperature());
+
+  }
+  @Test
+  public void exceptionRaisedWhenGetForecastByUnavailableRegion() {
+    try {
+      forecaster.getForecastFor("York", "Monday");
+      fail("Region not available in Region.class should raise IllegalArgumentException");
+    } catch (IllegalArgumentException e) {
+
+    }
+  }
+  @Test
+  public void exceptionRaisedWhenGetForecastByUnavailableDay() {
+    try {
+      forecaster.getForecastFor("London", "Today");
+      fail("Day not available in Day.class should raise IllegalArgumentException");
+    } catch(IllegalArgumentException e) {
+
+    }
   }
 }
